b320f88758495c6d8f10fe0b0204161793145f1f
Ahmad <skaz269@aucklanduni.ac.nz>
2024-06-30 18:04:15 +1200

    first commit

---
+++ src/components/Portfolio.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Portfolio = () => {
+    return (
+        <>
+            <div className="my-4 py-4" id='portfolio'>
+                <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">Products</h2>
+                <div className='flex justify-center'>
+                    <div className='w-24 border-b-4 border-blue-900 mb-8'></div>
+                </div>
+
+                <div className="px-4" data-aos="fade-down" data-aos-delay="600">
+                    <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5">                            
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3 min-h-max">
+                            <div className="m-2 text-justify text-sm">
+                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Personalized AI Conversation System</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                Our Personalized AI Conversation System is designed to emulate the unique communication style of your loved ones. By integrating advanced AI with your cherished messages, we create a lifelike and comforting experience that helps you stay connected with those who have passed or whom you’ve lost contact with. This service ensures that memories remain vivid and interactions feel genuine, providing emotional solace and continuity.
+                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400 overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+                            <div className="m-2 text-justify text-sm">
+                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Memory Preservation Platform</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                Our Memory Preservation Platform offers a comprehensive solution for storing and organizing your precious memories. From text messages and emails to photos and voice notes, all your data is securely stored in the cloud. This platform allows you to easily access and revisit your memories anytime, from any device, ensuring that the essence of your loved ones is always within reach.                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                                
+                                {/* <ul className="border-t border-gray-300 py-8 space-y-6">
+                                    <li className="flex items-center space-x-2 px-8">
+                                        <span className="bg-blue-600 rounded-full p-1">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 text-white" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
+                                            </svg>
+                                        </span>
+                                        <span className="text-gray-600 capitalize">Free Setup Guidance</span>
+                                    </li>
+                                </ul> */}
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+                            <div className="m-2 text-justify text-sm">
+                            <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Conversational AI Fine-Tuning Service</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                Our Conversational AI Fine-Tuning Service customizes the AI based on your specific needs. By utilizing messages, emails, and other personal communications, we fine-tune our AI to perfectly mirror the conversational style of your loved ones. This tailored approach ensures the most authentic and meaningful interactions, providing a deeply personalized experience.
+                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3">
+                            <div className="m-2 text-justify text-sm">
+                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Customized Emotional Support Solutions</h4>
+                                <p className="text-md font-medium leading-5 h-auto md:h-48">
+                                Our Customized Emotional Support Solutions provide personalized assistance to help you navigate the emotional complexities of loss and reconnection. Whether you need someone to talk to, or strategies for coping with grief, our AI-driven solutions are designed to offer comfort, understanding, and guidance, tailored to your unique situation.
+                                </p>
+                                <div className="flex justify-center my-4">
+                                    <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
+                                        Schedule Demo
+                                        <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>                    
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Portfolio;
---
+++ src/components/Services.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import img from '../images/web.svg'; 
+import img2 from '../images/app.svg';
+import img3 from '../images/hosting.svg';
+
+const Services = () => {
+
+    return (
+        <div id="services" className="bg-gray-100 py-12" >
+            <section data-aos="zoom-in-down">
+                    <div className="my-4 py-4">
+                        <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">services</h2>
+                        
+                        <div className='flex justify-center'>
+                            <div className='w-24 border-b-4 border-blue-900'></div>
+                        </div>
+                        <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">How It Works </h2>
+                    </div>
+
+                    <div className="px-12" data-aos="fade-down" data-aos-delay="600">
+                        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                            
+                            <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
+                                <div className="m-2 text-justify text-sm">
+                                <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img} />
+                                    <h2 className="font-semibold my-4 text-2xl text-center">Personalized AI Conversations:</h2>
+                                    <p className="text-md font-medium">
+                                    Our advanced AI technology is fine-tuned with messages, emails, and other communications from your loved ones to create a unique, personalized conversational experience.                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
+                                <div className="m-2 text-justify text-sm">
+                                <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img2} />
+                                    <h2 className="font-semibold my-4 text-2xl text-center">Retrieval-Augmented Generation (RAG):</h2>
+                                    <p className="text-md font-medium">
+                                    We use Retrieval-Augmented Generation to enhance the authenticity of interactions, ensuring the AI responds with contextually relevant and meaningful replies.                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
+                                <div className="m-2 text-justify text-sm">
+                                    <img alt="card img" className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out" src={img3} />
+                                    <h2 className="font-semibold my-4 text-2xl text-center ">Privacy and Ethics:</h2>
+                                    <p className="text-md font-medium">
+                                    Your privacy is our top priority. We follow strict ethical guidelines and ensure all data is used responsibly and with full consent.                                    </p>
+                                </div>
+                            </div>
+
+                                             
+                        </div>
+                    </div>
+            </section>
+
+            <section>
+                <div className="m-auto max-w-6xl p-2 md:p-12 h-5/6">
+                    <div className="flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left" data-aos="zoom-out">
+                        <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
+                            <div className='text-blue-900 mb-4'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24" className='fill-current'><path d="M2 12h2a7.986 7.986 0 0 1 2.337-5.663 7.91 7.91 0 0 1 2.542-1.71 8.12 8.12 0 0 1 6.13-.041A2.488 2.488 0 0 0 17.5 7C18.886 7 20 5.886 20 4.5S18.886 2 17.5 2c-.689 0-1.312.276-1.763.725-2.431-.973-5.223-.958-7.635.059a9.928 9.928 0 0 0-3.18 2.139 9.92 9.92 0 0 0-2.14 3.179A10.005 10.005 0 0 0 2 12zm17.373 3.122c-.401.952-.977 1.808-1.71 2.541s-1.589 1.309-2.542 1.71a8.12 8.12 0 0 1-6.13.041A2.488 2.488 0 0 0 6.5 17C5.114 17 4 18.114 4 19.5S5.114 22 6.5 22c.689 0 1.312-.276 1.763-.725A9.965 9.965 0 0 0 12 22a9.983 9.983 0 0 0 9.217-6.102A9.992 9.992 0 0 0 22 12h-2a7.993 7.993 0 0 1-.627 3.122z"></path><path d="M12 7.462c-2.502 0-4.538 2.036-4.538 4.538S9.498 16.538 12 16.538s4.538-2.036 4.538-4.538S14.502 7.462 12 7.462zm0 7.076c-1.399 0-2.538-1.139-2.538-2.538S10.601 9.462 12 9.462s2.538 1.139 2.538 2.538-1.139 2.538-2.538 2.538z"></path></svg>
+                            </div>
+                            <h3 className="text-3xl  text-blue-900 
+                            font-bold"><span className='font-black'>Authentic </span>Conversations:</h3>
+                            <div>
+                                <p className='my-3 text-xl text-gray-600 font-semibold'>
+                                Experience conversations that feel real and emotionally resonant, bringing back the unique voice and personality of your loved ones.                                </p>
+                            </div>
+                        </div>
+                        <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
+                            <div className='text-blue-900 mb-4'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24" fill="none" stroke="#1e3a8a" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg>                            
+
+                            </div>
+                            <h3 className="text-3xl  text-blue-900 font-bold">Safe and <span className='font-black'>Respectful</span>:</h3>
+                            <div>
+                                <p className='my-3 text-xl text-gray-600 font-semibold'>
+                                Our platform is designed to be a safe space for remembering and reconnecting, with strict adherence to privacy and ethical standards.                                    </p>
+                            </div>
+                        </div>
+                        <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
+                            <div className='text-blue-900 mb-4'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24" className='fill-current'><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg>
+
+                            </div>
+                            <h3 className="text-3xl  text-blue-900 font-bold">Continuous<span className='font-black'> Improvement</span></h3>
+                            <div>
+                                <p className='my-3 text-xl text-gray-600 font-semibold'>We constantly refine our AI to ensure the highest quality of interactions, based on your feedback and ongoing advancements in technology.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Services;
